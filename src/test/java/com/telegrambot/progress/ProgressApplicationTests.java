b096d77b4288030adad52a15794043b6d734fca6
Dmitry Skorobogatov <disco.it98@gmail.com>
2024-02-15 20:15:58 +0300

    Begin

---
+++ src/test/java/com/telegrambot/progress/ProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegrambot.progress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
