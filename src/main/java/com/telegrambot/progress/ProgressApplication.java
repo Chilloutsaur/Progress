1fb8b146db5e4f2e314f32929679abfff2994c21
Ruslan Kapnin <degogrand888@gmail.com>
2024-02-16 21:07:20 +0300

    fixed properties

--- src/main/java/com/telegrambot/progress/ProgressApplication.java
+++ src/main/java/com/telegrambot/progress/ProgressApplication.java
@@ -15,10 +15,10 @@ public class ProgressApplication {
         ProgressService progressService = SpringApplication.run(ProgressApplication.class, args).getBean(ProgressService.class);
         progressService.createPerson(1L, "dego");
         progressService.addGoals(1L, List.of(new Goal("nihuya ne delat"), new Goal("ne delat nihuya "), new Goal("ne delat nihuya nikogda")));
-        progressService.addAchievement(1L, LocalDateTime.MIN, 0L);
-        progressService.addAchievement(1L, LocalDateTime.MIN, 1L);
-        progressService.addAchievement(1L, LocalDateTime.MIN, 2L);
-        System.out.println(progressService.getCompletedGoalsByDate(1L, LocalDateTime.MIN));
+//        progressService.addAchievement(1L, LocalDateTime.MIN, 0L);
+//        progressService.addAchievement(1L, LocalDateTime.MIN, 1L);
+//        progressService.addAchievement(1L, LocalDateTime.MIN, 2L);
+//        System.out.println(progressService.getCompletedGoalsByDate(1L, LocalDateTime.MIN));
     }
 
 }
